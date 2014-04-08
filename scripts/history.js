d5da491c253f23a306e11795ceff9192e0071a40
ThomasAy <thomas@phonations.com>
2014-04-08 19:47:05 +0200

    ajout de l'historique

---
+++ scripts/history.js
@@ -0,0 +1,38 @@
+function History(){
+
+};
+
+History.prototype.addPage = function() {
+	// Récupère l'ancien historique
+
+	var h = new Object();
+	if(localStorage.getItem("history") != null)
+	{
+		h = JSON.parse(localStorage.getItem("history"));
+	}
+
+	console.log(h);
+
+	var parser = document.createElement('a');
+	parser.href = document.getElementById('omnibar').value;
+
+
+	if( h[parser.hostname] == undefined)
+	{
+		h[parser.hostname] = new Object();
+	}
+	h[parser.hostname][parser.href] = new Date();
+
+	window.test = h;
+	console.log(h);
+
+	console.log(JSON.stringify(h));
+    localStorage.setItem("history", JSON.stringify(h));
+	
+};
+
+
+History.prototype.getObject = function() {
+    var value = localStorage.getItem("history");
+    return value && JSON.parse(value);
+}
