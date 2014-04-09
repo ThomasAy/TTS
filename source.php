ed41d58478db4b98e77c548ff3b7ea152f6246e2
skyzobear <alexis.skyzobear.machado@gmail.com>
2014-04-09 10:14:50 +0200

    MAJ
    sons char

--- source.php
+++ source.php
@@ -2,10 +2,10 @@
     set_time_limit(0);
     $tab = array(65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, "i grec" => 89, 90, 
                 "point d'exclamation" => 33, 34, 35, 
-                36, 37, 38, 39, 40, 41, 42, 43, 44, "moins" => 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 
+                36, "gauche" => 37, "haut" => 38, "droite" => 39, "bas" => 40, 41, 42, 43, 44, "moins" => 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 
                 "barre verticale" => 124, "Retour arrière" => 8, "Espace" => 32, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 
-                109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, "i grec" => 121, 122, 'barre oblique' => 191, 'controle' => 17,
-                'shift' => 16, 'majuscule' => 20, 'tab' => 9, 'point' => 190);
+                109, 110, 'barre oblique' => 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, "i grec" => 121, 122, 'deux points' => 191, 'controle' => 17,
+                'shift' => 16, 'majuscule' => 20, 'tab' => 9, 'point' => 190, "entrée" =>13);
     $phrases = array('Bienvenu sur votre logiciel sarah','Bonjour je suis Homer','Bonjour je suis Julie','Bonjour je suis Murphy',
                     'chargement','choix de la voix','désolé votre adresse n\'est pas reconnue','Liste des favoris','précédent',
                     'suivant','veuillez patienter, recherche en cours','titre de niveau 1','titre de niveau 2','titre de niveau 3',
